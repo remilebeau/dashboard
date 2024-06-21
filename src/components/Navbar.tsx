e527cbe44924d2657da8734a3d7067a013482f90
remilebeau <remilebeau90@gmail.com>
2024-06-21 17:37:08 -0400

    moved ThemeToggle next to Profile picture

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,30 +15,32 @@ import { ThemeToggle } from "./ThemeToggle";
 export default function Navbar() {
   return (
     <nav className="flex justify-between bg-primary px-5 py-2">
-      <Link href="/">
-        <Image src={mario} alt="mario" width={40} />
-      </Link>
-
-      <ThemeToggle />
-
-      <DropdownMenu>
-        <DropdownMenuTrigger className="focus:outline-none">
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback className="text-primary">SC</AvatarFallback>
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <Link href="/profile">Profile</Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link href="/auth">Logout</Link>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <section>
+        <Link href="/">
+          <Image src={mario} alt="mario" width={40} />
+        </Link>
+      </section>
+      <section className="flex items-center justify-center gap-8">
+        <ThemeToggle />
+        <DropdownMenu>
+          <DropdownMenuTrigger className="focus:outline-none">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback className="text-primary">SC</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="/profile">Profile</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/auth">Logout</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </section>
     </nav>
   );
 }
